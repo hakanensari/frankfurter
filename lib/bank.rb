a0a981e897401cf68af8ce45bc0948c74ebd512e
Hakan Ensari <hakanensari@gmail.com>
2018-03-22 08:49:32 +0000

    Use ON CONFLICT when fetching all records

--- lib/bank.rb
+++ lib/bank.rb
@@ -5,10 +5,7 @@ require 'bank/feed'
 
 module Bank
   def self.fetch_all_rates!
-    Currency.db.transaction do
-      Currency.dataset.delete
-      Currency.multi_insert(Feed.historical.to_a)
-    end
+    Currency.dataset.insert_conflict.multi_insert(Feed.historical.to_a)
   end
 
   def self.fetch_current_rates!
@@ -17,5 +14,10 @@ module Bank
         Currency.find_or_create(hsh)
       end
     end
+  end
+
+  def self.replace_all_rates!
+    Currency.dataset.delete
+    Currency.multi_insert(Feed.historical.to_a)
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -13,7 +13,6 @@ describe Bank do
 
   before do
     VCR.insert_cassette 'feed'
-    Currency.dataset.delete
   end
 
   after do
@@ -21,12 +20,26 @@ describe Bank do
   end
 
   it 'fetches all rates' do
+    Currency.dataset.delete
     Bank.fetch_all_rates!
     Currency.count.must_be :positive?
   end
 
+  it 'skips existing records when fetching all rates' do
+    Currency.where { date < '2012-01-01' }.delete
+    Bank.fetch_all_rates!
+    Currency.where { date < '2012-01-01' }.count.must_be :positive?
+  end
+
   it 'fetches current rates' do
+    Currency.dataset.delete
     Bank.fetch_current_rates!
+    Currency.count.must_be :positive?
+  end
+
+  it 'replaces all rates' do
+    Currency.dataset.delete
+    Bank.fetch_all_rates!
     Currency.count.must_be :positive?
   end
 end
