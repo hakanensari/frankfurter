08d7efb72c8255ce17597cf51a9b02b8ff2b1d77
Hakan Ensari <hakanensari@gmail.com>
2020-05-02 19:42:53 +0100

    Remove obsolete file

--- config/sitemap.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-SitemapGenerator::Sitemap.default_host = 'https://frankfurter.app'
-SitemapGenerator::Sitemap.public_path = 'lib/web/public'
-SitemapGenerator::Sitemap.create
