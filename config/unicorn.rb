5252cc7742143eb91c522dc389c43689ce179b7c
Pål <pal@subtree.se>
2021-05-20 23:09:18 +0200

    build: Increase timeout as per Herokus recommendation

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ worker_process_count = (ENV['WORKER_PROCESSES'] || 4).to_i
 
 preload_app true
 worker_processes worker_process_count
-timeout 10
+timeout 15
 
 initialized = false
 before_fork do |_server, _worker|
